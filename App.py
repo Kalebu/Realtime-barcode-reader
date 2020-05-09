e9b754fb7433ec61be6c3dd272361c54d2eae137
Kalebu Gwalugano <kalebjordan.kj@gmail.com>
2020-05-09 13:36:36 +0300

    Initial-commit

---
+++ App.py
@@ -0,0 +1,27 @@
+import cv2
+from pyzbar import pyzbar
+
+def read_barcodes(frame):
+    barcodes = pyzbar.decode(frame)
+    for barcode in barcodes:
+        x, y , w, h = barcode.rect
+        barcode_text = barcode.data.decode('utf-8')
+        print(barcode_text)
+        cv2.rectangle(frame, (x, y),(x+w, y+h), (0, 255, 0), 2)
+    return frame
+
+def main():
+    camera = cv2.VideoCapture(0)
+    ret, frame = camera.read()
+    while ret:
+        ret, frame = camera.read()
+        frame = read_barcodes(frame)
+        cv2.imshow('Barcode reader', frame)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+
+    camera.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
